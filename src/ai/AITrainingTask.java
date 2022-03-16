ff688d45b965b4ee2935541a2b24e09fe7616272
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-03-16 17:18:55 +0100

    Increase loop condition for test purpose

--- src/ai/AITrainingTask.java
+++ src/ai/AITrainingTask.java
@@ -14,11 +14,11 @@ public class AITrainingTask extends Task<Integer> {
 		
 		int progressStatus = 0;
 		
-		for (int i=0; i < 10000000; i++) {
+		for (int i=0; i < 100000000; i++) {
 			
 			progressStatus = i;
 			
-			this.updateProgress(progressStatus, 10000000);
+			this.updateProgress(progressStatus, 100000000);
 			
 		}
 		
