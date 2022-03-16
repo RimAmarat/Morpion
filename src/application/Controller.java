525405a06a6f464c04186d7554b27f915ebdc7b3
RimAmarat <amaratrim@gmail.com>
2022-03-16 05:12:50 +0100

    modified:   bin/LoadAI.fxml 	modified:   bin/application/Controller$1.class 	modified:   bin/application/Controller.class 	modified:   src/LoadAI.fxml 	modified:   src/application/Controller.java

--- src/application/Controller.java
+++ src/application/Controller.java
@@ -14,9 +14,11 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.scene.control.Label;
 import javafx.scene.control.ProgressBar;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
 
 
-public class Controller {
+public class Controller implements Initializable {
 	
 	@FXML
     private ProgressBar progressBar = new ProgressBar();
@@ -27,32 +29,15 @@ public class Controller {
 	private double progress;
 	private Stage stage;
 	private Scene scene;
-	
-	Task<Integer> task = new Task<Integer>() {
-		@Override protected Integer call() throws Exception {
-             int iterations;
-             for (iterations = 0; iterations < 100; iterations++) {
-                 if (isCancelled()) {
-                     updateMessage("Cancelled");
-                     break;
-                 }
-                 System.out.println("Iteration " + iterations);
-                 updateMessage("Iteration " + iterations);
-                 updateProgress(iterations, 100);
-             }
-             return iterations;
-         }
-     };
-     /*
+     
 	@Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
 		// TODO Auto-generated method stub
-		progress = 0.2;
+		progress = 0.1;
 		progressBar.setProgress(progress);
 		progressLabel.setText(Double.toString(progress*100)+" %");
-
 	}
-	*/
+	
 	public void switchToChooseOpp(ActionEvent event) {
 		try {
 			Parent root = FXMLLoader.load(getClass().getResource("/ChooseOpponent.fxml"));
@@ -80,6 +65,7 @@ public class Controller {
 		}
 	}
 	
+	
 	public void loadAI() {
 		System.out.println("Début ");
 		progress = 0.5;
@@ -91,11 +77,31 @@ public class Controller {
 	
 	public void switchToLoadAI(ActionEvent event) {
 		try {
+
+			Task<Integer> task = new Task<Integer>() {
+				@Override protected Integer call() throws Exception {
+		             int iterations;
+		             for (iterations = 0; iterations < 100; iterations++) {
+		                 if (isCancelled()) {
+		                     updateMessage("Cancelled");
+		                     break;
+		                 }
+		                 System.out.println("Iteration " + iterations);
+		                 updateMessage("Iteration " + iterations);
+		                 updateProgress(iterations, 100);
+		             }
+		             return iterations;
+		         }
+		     };
+		     
+		    AnchorPane root = new AnchorPane();
+		    root = FXMLLoader.load(getClass().getResource("/LoadAI.fxml"));
+		    root.getChildren().get(2).accessibleTextProperty().bind(task.messageProperty());
+		    System.out.print("txt property -> "+root.getChildren().get(2).accessibleTextProperty());
 		    Thread th = new Thread(task);
 		    th.setDaemon(true);
+			// progressBar.accessibleTextProperty().bind(task.messageProperty());
 		    th.start();
-		    progressLabel.textProperty().bind(task.messageProperty());
-		    Parent root = FXMLLoader.load(getClass().getResource("/LoadAI.fxml"));
 		    stage = (Stage)((Node)event.getSource()).getScene().getWindow();
 			scene = new Scene(root);
 			stage.setScene(scene);
