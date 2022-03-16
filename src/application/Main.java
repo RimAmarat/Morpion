86f6e391cb68e859bc1efca217c0d6b6793f0ebd
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-03-16 17:01:30 +0100

    Adds a way to test the AI Training view and controller

--- src/application/Main.java
+++ src/application/Main.java
@@ -2,35 +2,30 @@ package application;
 	
 import javafx.application.Application;
 import javafx.stage.Stage;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
 import javafx.fxml.FXMLLoader;
 
 
 public class Main extends Application {
 	
 	@Override
-	public void start(Stage primaryStage) {
+	public void start(Stage stage) {
 		
 		try {
 			
 			// create scene
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
-			Scene scene = new Scene(root,400,400);
-			
-			// set style
-			scene
-				.getStylesheets()
-				.add(getClass().getResource("application.css").toExternalForm());
+			Parent root = FXMLLoader.load(getClass().getResource("ViewAITraining.fxml"));
+			Scene scene = new Scene(root, 400, 400);
 			
 			// display scene
-			primaryStage.setScene(scene);
-			primaryStage.show();
+			stage.setScene(scene);
+			stage.show();
+		
+		} catch(Exception exception) {
 			
-		} catch(Exception e) {
-			
-			e.printStackTrace();
-			
+			exception.printStackTrace();
+		
 		}
 		
 	}
