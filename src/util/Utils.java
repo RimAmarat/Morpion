bd26e5ad720970844b49b3bcf6af7132621cf4f3
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-04-08 11:28:04 +0200

    Adds Utils class to manage stage switching

---
+++ src/util/Utils.java
@@ -0,0 +1,60 @@
+package util;
+
+import java.io.IOException;
+
+// lib
+import javafx.application.Application;
+
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.Node;
+
+import javafx.stage.Stage;
+
+import javafx.fxml.FXMLLoader;
+
+import javafx.event.ActionEvent;
+
+
+public class Utils extends Application {
+	
+	private static Stage primaryStage;
+	
+	/**
+	 * Sets the primary stage
+	 * 
+	 * @param primaryStage - the primary stage
+	 */
+	public void setPrimaryStage(Stage primaryStage) { Utils.primaryStage = primaryStage; }
+	
+	/**
+	 * Returns the application's primary stage
+	 */
+	public Stage getPrimaryStage() { return Utils.primaryStage; }
+	
+	/**
+	 * Changes the current view
+	 * 
+	 * @param view - the path of the new view
+	 */
+	public void switchView(String view, ActionEvent event) {
+		
+		try {
+		
+			Parent root = FXMLLoader.load(getClass().getResource(view));
+			Scene scene = new Scene(root);
+			
+			Stage primaryStage = getPrimaryStage();
+			primaryStage.setScene(scene);
+			primaryStage.show();
+			
+			
+		} catch (IOException exception) {
+			
+			System.out.println(exception.getMessage());
+			
+		}
+		
+	}
+
+}
