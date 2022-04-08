4eac2b6cdfb967c1017336040e008d8d047fd25d
Lahcène Belhadi <lahcene.belhadi@gmail.com>
2022-04-08 13:01:24 +0200

    Adds checkWin method, need to finish impl

--- src/application/Game.java
+++ src/application/Game.java
@@ -2,6 +2,8 @@ package application;
 
 import java.lang.IllegalArgumentException;
 
+import java.util.Optional;
+
 
 public class Game {
 
@@ -43,7 +45,7 @@ public class Game {
 	 * @param y - the y coordinate
 	 * @param value - the value to put in the cell
 	 */
-	public void setCellValue(int x, int y, int value) throws IllegalArgumentException {
+	public Optional<Integer> setCellValue(int x, int y, int value) throws IllegalArgumentException {
 		
 		if (x < 0 || y < 0)
 			throw new IllegalArgumentException("The value of x and y must be positive.");
@@ -52,6 +54,65 @@ public class Game {
 			throw new IllegalArgumentException("The value of the cell must be -1 or 1.");
 		
 		grid[x][y] = value;
+		
+		return checkWin();
+		
+	}
+	
+	/**
+	 * Checks if there is a winner and returns the id of the winner
+	 * 
+	 * Returns 1 for the X and -1 for the O
+	 */
+	public Optional<Integer> checkWin() {
+		
+		Optional<Integer> winner = Optional.empty();
+
+		int countX = 0;
+		int countO = 0;
+		
+		// test rows
+		for (int i=0; i < 3; i++) {
+			
+			for (int j=0; j < 3; j++) {
+				
+				// adds point to X player
+				if (grid[i][j] == -1)
+					countX++;
+				
+				// adds point to O player
+				else if (grid[i][j] == 1)
+					countO++;
+				
+			}
+			
+			// if player X has enough points
+			if (countX == 3) {
+				
+				// sets player X as winner
+				winner = Optional.of(-1);
+				break;
+				
+			} else if (countO == 3) {
+				
+				winner = Optional.of(1);
+				break;
+				
+			}
+			
+			// go to nex row
+			countX = 0;
+			countO = 0;
+			
+		}
+		
+		// test columns
+		
+		// test diag right to left
+		
+		// test diag left to right
+		
+		return winner;
 		
 	}
 	
